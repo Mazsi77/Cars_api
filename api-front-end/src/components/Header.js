7a3610f7c440c321c62ee40ff927300350923c81
Szekely Mihaly-Zsolt <zsoltszekely8@gmail.com>
2022-01-19 22:28:52 +0200

    Prezi

--- api-front-end/src/components/Header.js
+++ api-front-end/src/components/Header.js
@@ -7,10 +7,10 @@ export const Header = () => {
     return (
         <nav>
             <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
+            <AppBar position="static" style={{marginBottom: 10}}>
                 <Toolbar sx={{maxWidth : 1200, margin: 'auto'}}>
-                    <MaterialLink component={RouterLink} to='/brands'>Brands</MaterialLink>
-                    <MaterialLink component={RouterLink} to='/models'>Models</MaterialLink>
+                    <MaterialLink component={RouterLink} underline="hover" padding="10px" to='/brands'>Brands</MaterialLink>
+                    <MaterialLink component={RouterLink} underline="hover" padding="10px" to='/models'>Models</MaterialLink>
                 </Toolbar>
             </AppBar>
             </Box>
